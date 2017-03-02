51f1bc3a997a6992d5abebf1a9b98aa99c9a1a68
韩励智 <hanlizhi@hotmail.com>
2017-03-02 23:57:23 +0800

    complete all repository

--- src/main/java/com/hlz/TestApplication.java
+++ src/main/java/com/hlz/TestApplication.java
@@ -1,22 +1,13 @@
 package com.hlz;
 
-import com.hlz.config.AuthorSettings;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class TestApplication {
-    @Autowired
-    private AuthorSettings authorSettings;
-    
-    @RequestMapping("/")
-    String index(){
-        return "author name is"+authorSettings.getName()+"author age is:"+authorSettings.getAge();
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
     }
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
 }
