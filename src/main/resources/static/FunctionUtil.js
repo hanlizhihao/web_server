cecba92fdb738ade629fd6162ba462e83afa827a
韩励智 <hanlizhi@hotmail.com>
2017-03-17 20:06:53 +0800

    Repair the string parsing problem

--- src/main/resources/static/FunctionUtil.js
+++ src/main/resources/static/FunctionUtil.js
@@ -4,18 +4,28 @@
 function timeToString(wait) {
     var waitTime = new String();
     if (wait >= 60 && wait < 3600) {
-        var minute = Math.round(wait / 60);
-        var second = Math.round(wait % 60);
+        var minute = round(wait/60);
+        var second = wait%60;;
         waitTime = minute + "分钟" + second + "秒";
     } else if (wait < 60) {
         waitTime = wait + "秒";
     } else if (wait >= 3600) {
-        var hour = Math.round(wait / 3600);
-        var minute = Math.round(wait % 3600 / 60);
-        var second = Math.round(wait % 3600 % 60);
+        var hour = round(wait / 3600);
+        var minute = round(wait % 3600/60);
+        var second = wait % 3600 % 60;
         waitTime = hour + "小时" + minute + "分钟" + second + "秒";
     }
     return waitTime;
+}
+//由于Math.round函数是四舍五入的，所以不符合需求，需要向下取整的方法
+function round(number){
+    var num=Number(number);
+    var numRound=Math.round(num);
+    if(numRound>num){
+        return numRound-1;
+    }else{
+        return numRound;
+    }
 }
 //连接stomp
 function connectStomp() {
